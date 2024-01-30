2b57d660226843b3b5ce6d43a671bbe002de5a2b
Alexey Staburov <gilbertfrost@yandex.ru>
2024-01-30 22:14:29 +0300

    layout search go

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-
+  <div class="container container-search">
+    <nav>
+      <div class="nav-wrapper">
+        <form class="">
+          <div class="input-field">
+            <input id="search" type="search" required>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+    </nav>
   </div>
 </template>
 
