c6f11b5fc57b2f286fe4989369d890675a408b95
Alexey Staburov <gilbertfrost@yandex.ru>
2024-01-31 18:20:03 +0300

    card layout done

---
+++ src/components/CardBeer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="card black-text">
+      <div class="card-image">
+        <img class="image-card activator"
+          src="https://leolsbeer.ru/upload/iblock/c4d/heineken-lager-0-65-l-alk-5-0.png" />
+        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+      </div>
+      <div class="card-content">
+        <span class="card-title">Название пива</span>
+        <div class="card-abv-ibu">
+          <p>ABV: 6</p>
+          <p>IBU: 70</p>
+        </div>
+        <div class="chip">
+          Post Modern Classic
+        </div>
+        <div class="chip">
+          Spiky
+        </div>
+        <div class="chip">
+          Tropical
+        </div>
+        <div class="chip">
+          Hoppy
+        </div>
+        <div class="card-action">
+          <p><a href="#" class="activator">Подробнее</a></p>
+          <p><a href="#">Открыть рецепт</a></p>
+        </div>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/FavoritePage.vue
+++ src/views/FavoritePage.vue
@@ -1,93 +1,17 @@
 <template>
   <div class="row container cards">
-    <div class="card">
-      <div class="card-image">
-        <img
-          class="image-card"
-          src="https://leolsbeer.ru/upload/iblock/c4d/heineken-lager-0-65-l-alk-5-0.png"
-        />
-
-        <a class="btn-floating halfway-fab waves-effect waves-light red"
-          ><i class="material-icons">add</i></a
-        >
-      </div>
-      <div class="card-content">
-        <span class="card-title black-text">Card Title</span>
-        <span class="card-title black-text">Card Title</span>
-        <p>
-          I am a very simple card. I am good at containing small bits of
-          information. I am convenient because I require little markup to use
-          effectively.
-        </p>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-image">
-        <img
-          class="image-card"
-          src="https://leolsbeer.ru/upload/iblock/c4d/heineken-lager-0-65-l-alk-5-0.png"
-        />
-
-        <a class="btn-floating halfway-fab waves-effect waves-light red"
-          ><i class="material-icons">add</i></a
-        >
-      </div>
-      <div class="card-content">
-        <span class="card-title black-text">Card Title</span>
-        <p>
-          I am a very simple card. I am good at containing small bits of
-          information. I am convenient because I require little markup to use
-          effectively.
-        </p>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-image">
-        <img
-          class="image-card"
-          src="https://leolsbeer.ru/upload/iblock/c4d/heineken-lager-0-65-l-alk-5-0.png"
-        />
-
-        <a class="btn-floating halfway-fab waves-effect waves-light red"
-          ><i class="material-icons">add</i></a
-        >
-      </div>
-      <div class="card-content">
-        <span class="card-title black-text">Card Title</span>
-        <span class="card-title black-text">Card Title</span>
-        <p>
-          I am a very simple card. I am good at containing small bits of
-          information. I am convenient because I require little markup to use
-          effectively.
-        </p>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-image">
-        <img
-          class="image-card"
-          src="https://leolsbeer.ru/upload/iblock/c4d/heineken-lager-0-65-l-alk-5-0.png"
-        />
-
-        <a class="btn-floating halfway-fab waves-effect waves-light red"
-          ><i class="material-icons">add</i></a
-        >
-      </div>
-      <div class="card-content">
-        <span class="card-title black-text">Card Title</span>
-        <span class="card-title black-text">Card Title</span>
-        <p>
-          I am a very simple card. I am good at containing small bits of
-          information. I am convenient because I require little markup to use
-          effectively.
-        </p>
-      </div>
-    </div>
+    <card-beer></card-beer>
+    <card-beer></card-beer>
+    <card-beer></card-beer>
+    <card-beer></card-beer>
   </div>
 </template>
 
 <script>
+import CardBeer from '@/components/CardBeer.vue';
 export default {
+  components: { CardBeer },
+
   data: () => ({
     cards: [
       {
